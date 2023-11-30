c29612cefe23220183e267f579429ed5eae699a3
Ahmed-Solutelabs <kaziahmed.r@solutelabs.com>
2023-11-30 14:32:06 +0530

    adding event in button

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
         <p>Ahmed kazi</p>
-        <button>Add+</button>
+        <button onClick={() => console.log('clicked')}>Add+</button>
     </div>
   );
 }
