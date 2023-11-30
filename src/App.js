9eca4a969cf5666b62137d03cd5c90ca40aef301
Ahmed-Solutelabs <kaziahmed.r@solutelabs.com>
2023-11-30 15:38:39 +0530

    adding api

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ function App() {
   return (
     <div className="App">
         <p>Ahmed kazi</p>
-        <button onClick={() => console.log('clicked')}>Add+</button>
+        <button onClick={() => fetch('https://mocki.io/v1/a91f45fd-e5b6-45c6-abfb-82654f28a56e')
+                 .then(response => response.json())
+                  .then(data => console.log(data))
+                  .catch(error => console.error(error))}>Add+</button>
     </div>
   );
 }
