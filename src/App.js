1121d730dc977f903fd974fb7bbc2de84bbe9fe4
Ahmed-Solutelabs <kaziahmed.r@solutelabs.com>
2023-12-04 15:43:19 +0530

    back to normal

--- src/App.js
+++ src/App.js
@@ -37,7 +37,22 @@ function App() {
     <div className="App">
       <p>Ahmed kazi</p>
       <button
-        onClick={() =>console.log("a") }
+        onClick={() =>  window.AP.request({
+    url: 'https://dhruvbabariya.atlassian.net/rest/api/3/issue/DHRUV-1',
+    type: 'GET',
+    headers: {
+                  Authorization: `Basic ${btoa(
+                    "dhruvbabariya912001@gmail.com:ATATT3xFfGF0grD_QGtfiXjWU-e_GYcZGfFW051EK7I3nQ9l7475uBrzE0WQzBJZvAglwVPyQD5Do2SI0HNWYQ8dtwgjxP4fS9vtN8OmiBQFy59wAyKfp7xg2lQFOp0KA1BiCGp3qsFz93fIb1lOKQnctbJnTO2eMCHDyuHy6X_veKenYVn1rnU=848280CE"
+                  )}`,
+                  Accept: "application/json",
+                },
+    success: function(responseText){
+      console.log(responseText);
+    },
+    error: function(xhr, statusText, errorThrown){
+      console.log(arguments);
+    }
+  }) }
       >
         Add+
       </button>
