72a318a969a9369f9e6f5f17369c5f4a7ecfd023
Ahmed-Solutelabs <kaziahmed.r@solutelabs.com>
2023-12-04 15:38:39 +0530

    back to normal

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
@@ -13,33 +13,31 @@ function App() {
   //   issueID = url[url.length - 1];
   // }
 
+  // A simple POST request which logs response in the console.
+  // AP.request({
+  //   url: 'https://dhruvbabariya.atlassian.net/rest/api/3/issue/DHRUV-1',
+  //   type: 'GET',
+  //   headers: {
+  //                 Authorization: `Basic ${btoa(
+  //                   "dhruvbabariya912001@gmail.com:ATATT3xFfGF0grD_QGtfiXjWU-e_GYcZGfFW051EK7I3nQ9l7475uBrzE0WQzBJZvAglwVPyQD5Do2SI0HNWYQ8dtwgjxP4fS9vtN8OmiBQFy59wAyKfp7xg2lQFOp0KA1BiCGp3qsFz93fIb1lOKQnctbJnTO2eMCHDyuHy6X_veKenYVn1rnU=848280CE"
+  //                 )}`,
+  //                 Accept: "application/json",
+  //               },
+  //   success: function(responseText){
+  //     console.log(responseText);
+  //   },
+  //   error: function(xhr, statusText, errorThrown){
+  //     console.log(arguments);
+  //   }
+  // });
+
+  console.log(window.AP.request)
 
   return (
     <div className="App">
       <p>Ahmed kazi</p>
       <button
-        onClick={() =>
-          fetch(
-            `https://dhruvbabariya.atlassian.net/rest/api/3/issue/DHRUV-1`,
-            {
-              method: "GET",
-              headers: {
-                Authorization: `Basic ${btoa(
-                  "dhruvbabariya912001@gmail.com:ATATT3xFfGF0grD_QGtfiXjWU-e_GYcZGfFW051EK7I3nQ9l7475uBrzE0WQzBJZvAglwVPyQD5Do2SI0HNWYQ8dtwgjxP4fS9vtN8OmiBQFy59wAyKfp7xg2lQFOp0KA1BiCGp3qsFz93fIb1lOKQnctbJnTO2eMCHDyuHy6X_veKenYVn1rnU=848280CE"
-                )}`,
-                Accept: "application/json",
-              },
-            }
-          )
-            .then((response) => {
-              console.log(
-                `Response: ${response.status} ${response.statusText}`
-              );
-              return response.text();
-            })
-            .then((text) => console.log(text))
-            .catch((err) => console.error(err))
-        }
+        onClick={() =>console.log("a") }
       >
         Add+
       </button>
