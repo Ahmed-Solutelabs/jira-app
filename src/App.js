b9aac53562aecf9c04ef91d4599493c805507526
Ahmed-Solutelabs <kaziahmed.r@solutelabs.com>
2023-12-04 12:53:59 +0530

    change in api call

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import "./App.css";
 function App() {
   const url = window.location.href.split("/");
   const domainName = url[2];
-  let issueID = ""
+  let issueID = "";
 
   const searchParams = new URLSearchParams(window.location.search);
-  if (searchParams.has('selectedIssue')) {
-    issueID = searchParams.get('selectedIssue');
-  }else {
-    issueID = url[url.length-1];
+  if (searchParams.has("selectedIssue")) {
+    issueID = searchParams.get("selectedIssue");
+  } else {
+    issueID = url[url.length - 1];
   }
 
   return (
@@ -23,9 +23,9 @@ function App() {
             {
               method: "GET",
               headers: {
-                Authorization: `Basic ${Buffer.from(
+                Authorization: `Basic ${btoa(
                   "dhruvbabariya912001@gmail.com:ATATT3xFfGF0grD_QGtfiXjWU-e_GYcZGfFW051EK7I3nQ9l7475uBrzE0WQzBJZvAglwVPyQD5Do2SI0HNWYQ8dtwgjxP4fS9vtN8OmiBQFy59wAyKfp7xg2lQFOp0KA1BiCGp3qsFz93fIb1lOKQnctbJnTO2eMCHDyuHy6X_veKenYVn1rnU=848280CE"
-                ).toString("base64")}`,
+                )}`,
                 Accept: "application/json",
               },
             }
