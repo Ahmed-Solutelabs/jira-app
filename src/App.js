f29097b91b38bdd7cb18015841724ca059c79f17
Ahmed-Solutelabs <kaziahmed.r@solutelabs.com>
2023-12-04 16:17:50 +0530

    api

--- src/App.js
+++ src/App.js
@@ -1,61 +1,14 @@
-// import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  // const url = window.location.href.split("/");
-  // const domainName = url[2];
-  // let issueID = "";
-
-  // const searchParams = new URLSearchParams(window.location.search);
-  // if (searchParams.has("selectedIssue")) {
-  //   issueID = searchParams.get("selectedIssue");
-  // } else {
-  //   issueID = url[url.length - 1];
-  // }
-
-  // A simple POST request which logs response in the console.
-  // AP.request({
-  //   url: 'https://dhruvbabariya.atlassian.net/rest/api/3/issue/DHRUV-1',
-  //   type: 'GET',
-  //   headers: {
-  //                 Authorization: `Basic ${btoa(
-  //                   "dhruvbabariya912001@gmail.com:ATATT3xFfGF0grD_QGtfiXjWU-e_GYcZGfFW051EK7I3nQ9l7475uBrzE0WQzBJZvAglwVPyQD5Do2SI0HNWYQ8dtwgjxP4fS9vtN8OmiBQFy59wAyKfp7xg2lQFOp0KA1BiCGp3qsFz93fIb1lOKQnctbJnTO2eMCHDyuHy6X_veKenYVn1rnU=848280CE"
-  //                 )}`,
-  //                 Accept: "application/json",
-  //               },
-  //   success: function(responseText){
-  //     console.log(responseText);
-  //   },
-  //   error: function(xhr, statusText, errorThrown){
-  //     console.log(arguments);
-  //   }
-  // });
-
-  console.log(window.AP.request)
-
   return (
     <div className="App">
-      <p>Ahmed kazi</p>
-      <button
-        onClick={() =>  window.AP.request({
-    url: 'https://dhruvbabariya.atlassian.net/rest/api/3/issue/DHRUV-1',
-    type: 'GET',
-    headers: {
-                  Authorization: `Basic ${btoa(
-                    "dhruvbabariya912001@gmail.com:ATATT3xFfGF0grD_QGtfiXjWU-e_GYcZGfFW051EK7I3nQ9l7475uBrzE0WQzBJZvAglwVPyQD5Do2SI0HNWYQ8dtwgjxP4fS9vtN8OmiBQFy59wAyKfp7xg2lQFOp0KA1BiCGp3qsFz93fIb1lOKQnctbJnTO2eMCHDyuHy6X_veKenYVn1rnU=848280CE"
-                  )}`,
-                  Accept: "application/json",
-                },
-    success: function(responseText){
-      console.log(responseText);
-    },
-    error: function(xhr, statusText, errorThrown){
-      console.log(arguments);
-    }
-  }) }
-      >
-        Add+
-      </button>
+        <p>Ahmed kazi</p>
+        <button onClick={() => fetch('https://mocki.io/v1/a91f45fd-e5b6-45c6-abfb-82654f28a56e')
+                 .then(response => response.json())
+                  .then(data => console.log(data))
+                  .catch(error => console.error(error))}>Add+</button>
     </div>
   );
 }
